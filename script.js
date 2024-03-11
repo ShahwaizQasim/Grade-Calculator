ce8aeccf953f095df46b4094a5457a895d99d56f
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-03-11 00:03:05 -0700

    assignment done

--- script.js
+++ script.js
@@ -4,27 +4,22 @@ let obtainmarks = document.querySelector("#obtain_marks");
 let showgrade = document.querySelector("#result");
 
 gradecheck.addEventListener("submit", (event) => {
-   event.preventDefault();
+  event.preventDefault();
 
-   let user_totalmarks = totalmarks.value;
-   let user_obtainmarks =  obtainmarks.value;
-   let percentage = (user_obtainmarks/user_totalmarks) * 100;
-   if (percentage >= 80) {
-      showgrade.innerText = "Congratulations ! Your Grade is A+ 😍"
-   }
-   else if(percentage >= 70){
-      showgrade.innerText = "Congratulations ! Your Grade is A 😍"
-   }
-   else if(percentage >= 60){
-    showgrade.innerText = "Your Grade is B"
-   }
-   else if(percentage >= 50){
-    showgrade.innerText = "Your Grade is C"
-   }
-   else if(percentage >= 40){
-    showgrade.innerText = "You are Fail ☹️"
-   }else{
-    showgrade.innerText = "You are Fail ☹️"
-   }
-
+  let user_totalmarks = totalmarks.value;
+  let user_obtainmarks = obtainmarks.value;
+  let percentage = (user_obtainmarks / user_totalmarks) * 100;
+  if (percentage >= 80) {
+    showgrade.innerText = "Congratulations ! Your Grade is A+ 😍";
+  } else if (percentage >= 70) {
+    showgrade.innerText = "Congratulations ! Your Grade is A 😍";
+  } else if (percentage >= 60) {
+    showgrade.innerText = "Your Grade is B";
+  } else if (percentage >= 50) {
+    showgrade.innerText = "Your Grade is C";
+  } else if (percentage >= 40) {
+    showgrade.innerText = "You are Fail ☹️";
+  } else {
+    showgrade.innerText = "You are Fail ☹️";
+  }
 });
